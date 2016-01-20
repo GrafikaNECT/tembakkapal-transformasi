170b82cd81b6c07b55635b97869ca39db3504ed2
Erick Chandra <erickchandra.1@gmail.com>
2016-01-20 18:27:54 +0700

    Update character A, B, C. Add hapusScreen().

--- src/mprintchar.c
+++ src/mprintchar.c
@@ -5,10 +5,10 @@ int main(){
 	initializePrinter();
 	int space = 2;
 	int i,j;
+	hapusScreen();
 	for (i=0;i<10;i++){
 		for (j=0;j<10;j++){
 			printChar('I',i*charpixmatrix_width+2,j*charpixmatrix_height+2,255,255,255,0);
 		}
-
 	}
 }
