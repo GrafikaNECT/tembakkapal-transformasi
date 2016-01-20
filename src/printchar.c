96c74e6a7302526f4726462d35b61f266de0dd59
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-01-20 18:17:22 +0700

    test

--- src/printchar.c
+++ src/printchar.c
@@ -60,27 +60,31 @@ int finishPrinter(){
 }
 
 void printChar(char a, int X, int Y, unsigned char R, unsigned char G, unsigned char B, unsigned char alpha){
+
+	charpixmatrix_type pixelmatrix = getcharpixmatrix(a);
     long int location = 0;
 // Figure out where in memory to put the pixel
     for (y = Y; y < charpixmatrix_height; y++)
         for (x = X; x < charpixmatrix_width; x++) {
+	    if (pixelmatrix.tab[y][x]){
 
-            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                       (y+vinfo.yoffset) * finfo.line_length;
+		    location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+		               (y+vinfo.yoffset) * finfo.line_length;
 
-            if (vinfo.bits_per_pixel == 32) {
-                *(fbp + location) = B;        // Some blue
-                *(fbp + location + 1) = G;     //  green
-                *(fbp + location + 2) = R;    //  red
-                *(fbp + location + 3) = alpha;      // alpha
-        //location += 4;
-            } else  { //assume 16bpp
-                int b = B/8;
-                int g = G/8;     // A little green
-                int r = R/8;    // A lot of red
-                unsigned short int t = r<<11 | g << 5 | b;
-                *((unsigned short int*)(fbp + location)) = t;
-            }
+		    if (vinfo.bits_per_pixel == 32) {
+		        *(fbp + location) = B;        // Some blue
+		        *(fbp + location + 1) = G;     //  green
+		        *(fbp + location + 2) = R;    //  red
+		        *(fbp + location + 3) = alpha;      // alpha
+		//location += 4;
+		    } else  { //assume 16bpp
+		        int b = B/8;
+		        int g = G/8;     // A little green
+		        int r = R/8;    // A lot of red
+		        unsigned short int t = r<<11 | g << 5 | b;
+		        *((unsigned short int*)(fbp + location)) = t;
+		    }
+		}
 
         }
     munmap(fbp, screensize);
