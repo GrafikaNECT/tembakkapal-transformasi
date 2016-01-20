de8d0079987d5cf35bfc220825756cd5deba4a88
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-01-20 17:49:15 +0700

    coba letterI

--- src/char.h
+++ src/char.h
@@ -1,4 +1,15 @@
 #ifndef char_h
 #define char_h
 
+#include <stdbool.h>
+
+#define charpixmatrix_width 8
+#define charpixmatrix_height 16
+
+typedef struct {
+	bool tab[charpixmatrix_height][charpixmatrix_width];
+}charpixmatrix_type;
+
+charpixmatrix_type getcharpixmatrix(char a);
+
 #endif
---
+++ src/mchar.c
@@ -0,0 +1,5 @@
+#include "char.h"
+
+int main(){
+	return 0;
+}
