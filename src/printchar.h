0a89fce9a653cfab0ed01420e94c5a03e84cf5c8
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-01-21 15:17:51 +0700

    resolve definition conflict printText()

--- src/printchar.h
+++ src/printchar.h
@@ -7,7 +7,7 @@ int finishPrinter();
 
 void printChar(char a, int X, int Y, int size, unsigned char R, unsigned char G, unsigned char B, unsigned char alpha);
 
-void printText(char* a, int alen, int X, int Y, unsigned char R, unsigned char G, unsigned char B, unsigned char alpha);
+void printText(char* a, int alen, int X, int Y, int size, unsigned char R, unsigned char G, unsigned char B, unsigned char alpha);
 
 void hapusScreen();
 
