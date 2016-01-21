6a0a642d049af382f86c33957ad9a487546cc50d
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-01-21 14:40:43 +0700

    Add names

--- src/mprintchar.c
+++ src/mprintchar.c
@@ -16,16 +16,21 @@ int main(){
 
 	for (i=400;i>0;i--){
 		printText("GRAPHICAT!!!",12,150,i,i%200,i%150,i%255,255);
-		printText("Asanilta Fahda",12,150,i-25,i%200,i%150,i%255,255);
-		printText("Candy Olivia M",14,150,i-50,i%200,i%150,i%255,255);
-		printText("Erick Chandra",13,150,i-75,i%200,i%150,i%255,255);
-		printText("M Nizami",9,150,i-100,i%200,i%150,i%255,255);
+		printText("Asanilta Fahda",14,150,i-25,i%200,i%150,i%255,255);
+		printText("Tifani Warnita",14,150,i-50,i%200,i%150,i%255,255);
+		printText("Candy Olivia M",14,150,i-75,i%200,i%150,i%255,255);
+		printText("Erick Chandra",13,150,i-100,i%200,i%150,i%255,255);
+		printText("M Nizami",8,150,i-125,i%200,i%150,i%255,255);
+		printText("Fathul Asrar A",14,150,i-150,i%200,i%150,i%255,255);
+
 		usleep(5000);
 		printText("GRAPHICAT!!!",12,150,i,255,255,255,255);
 		printText("Asanilta Fahda",14,150,i-25,255,255,255,255);
-		printText("Candy Olivia M",14,150,i-50,255,255,255,255);
-		printText("Erick Chandra",13,150,i-75,255,255,255,255);
-		printText("M Nizami",8,150,i-100,200,150,255,255);
+		printText("Tifani Warnita",14,150,i-50,255,255,255,255);
+		printText("Candy Olivia M",14,150,i-75,255,255,255,255);
+		printText("Erick Chandra",13,150,i-100,255,255,255,255);
+		printText("M Nizami",14,150,i-125,255,255,255,255);
+		printText("Fathul Asrar A",14,150,i-150,255,255,255,255);
 		//hapusScreen();
 	}
 
