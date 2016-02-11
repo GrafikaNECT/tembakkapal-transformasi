53ef37c1a5f4b211cef2fc6929a29e916e6b1eba
Erick Chandra <erickchandra.1@gmail.com>
2016-02-11 12:16:19 +0700

    Buat satu sayap baling-baling.

--- src/balingbaling.cpp
+++ src/balingbaling.cpp
@@ -21,6 +21,30 @@ void balingbaling::draw(){
 	polygon protate = p.hasilRotasi(rotateDegree);
 
 	protate.draw(x,y,255,255,0,255,true);
+
+	// Satu baling-baling atas
+	t0x = 0; t0y = 0;
+	t1x = width/12 * scale;
+	t1y = height/10 * scale;
+	t2x = width/6 * scale;
+	t2y = 3 * height/10 * scale;
+	t3x = width/12 * scale;
+	t3y = 4 * height/10 * scale;
+	t4x = 0;
+	t4y = height/2 * scale;
+
+	polygon p1;
+	p1.push_back(t0x, t0y);
+	p1.push_back(t1x, t1y);
+	p1.push_back(t2x, t2y);
+	p1.push_back(t3x, t3y);
+	p1.push_back(t4x, t4y);
+	p1.push_back(-t3x, t3y);
+	p1.push_back(-t2x, t2y);
+	p1.push_back(-t1x, t1y);
+	polygon protate1 = p1.hasilRotasi(rotateDegree);
+
+	protate1.draw(x,y,255,255,0,255,true);
 }
 void balingbaling::oneFrameMove(){
 	rotateDegree=(rotateDegree+rotateSpeed);
